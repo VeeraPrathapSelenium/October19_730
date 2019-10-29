6048463fc0785199c618a58f1a0fe4f25fbad7a8
tm <tm@SYS_1>
2019-10-29 08:55:30 +0530

    Conditional statements

---
+++ src/com/conditional_statements/SimpleIf.java
@@ -0,0 +1,28 @@
+package com.conditional_statements;
+
+public class SimpleIf {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		
+		int b=20;
+		
+		if(a>b){
+			System.out.println("A is greater than B");
+		}
+		else
+		{
+			System.out.println("A is not greater than B");
+		}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/com/conditional_statements/Task_1.java
@@ -0,0 +1,30 @@
+package com.conditional_statements;
+
+public class Task_1 {
+
+	public static void main(String[] args) {
+
+int a=20;
+
+int b=45;
+
+if(a>b)
+{
+	System.out.println("A is greater than B ");
+}else if(a>=10 && a<=40)
+{
+	System.out.println("A is not greater than B , but it is in the range of 10 and 40");
+}
+else
+{
+	System.out.println("A is not greater than B also not  in the range of 10 and 40 ");
+}
+
+
+
+		
+		
+
+	}
+
+}
