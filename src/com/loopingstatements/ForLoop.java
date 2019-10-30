b6a2cc80adb0018186e9ad70b8efe84831bd88e6
tm <tm@192.168.1.37>
2019-10-30 08:52:04 +0530

    Looping statements

---
+++ src/com/loopingstatements/ForLoop.java
@@ -0,0 +1,23 @@
+package com.loopingstatements;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+
+		int counter=0;
+		
+		for(int i=1;i<=20;i++)
+		{
+			System.out.println("Welcome"+i);
+			
+			counter++;
+		}
+		
+		System.out.println(counter);
+		
+		
+		
+
+	}
+
+}
---
+++ src/com/loopingstatements/Task_1.java
@@ -0,0 +1,29 @@
+package com.loopingstatements;
+
+import java.util.Scanner;
+
+public class Task_1 {
+
+	public static void main(String[] args) {
+
+
+	Scanner value=new Scanner(System.in);
+	
+	int userinput=value.nextInt();
+	
+	
+	for(int i=1;i<=20;i++)
+	{
+		
+		System.out.println(userinput +" * " +i+" = "+ userinput*i);
+		
+		
+		
+		
+	}
+		
+		
+
+	}
+
+}
