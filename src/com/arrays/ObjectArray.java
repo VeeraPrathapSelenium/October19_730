e693ee50cc5a30db0fd7e2668b697e90d2a934ac
tm <tm@192.168.1.37>
2019-11-04 08:55:23 +0530

    Arrays,Strings

---
+++ src/com/arrays/ObjectArray.java
@@ -0,0 +1,23 @@
+package com.arrays;
+
+public class ObjectArray {
+
+	public static void main(String[] args) {
+
+
+		Object[] arr=new Object[3];
+		
+		arr[0]="Raj";
+		arr[1]=123;
+		
+		for (Object object : arr) {
+			System.out.println(object);
+		}
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/com/strings/StringsDeclaration.java
@@ -0,0 +1,21 @@
+package com.strings;
+
+public class StringsDeclaration {
+
+	public static void main(String[] args) {
+
+		String str1="India 1234";
+		
+		String str2="India 1234";
+		
+		String s3=new String("India 1234");
+		String s4=new String("India 1234");
+		
+		System.out.println(str1==str2);
+		
+		
+		
+
+	}
+
+}
